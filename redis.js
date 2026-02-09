55cc16135448afc4a34d7b66f0484ac9f9e0a479
Dopumaru <caio.10silva@hotmail.com>
2026-02-09 20:46:01 -0300

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,9 +1,10 @@
-const IORedis = require("ioredis");
+const IORedis = require('ioredis');
 
-const connection = new IORedis({
-  host: "127.0.0.1",
-  port: 6379,
+const redis = new IORedis({
+  host: process.env.REDIS_HOST || 'redis-fila',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
   maxRetriesPerRequest: null,
 });
 
-module.exports = connection;
+module.exports = redis;
