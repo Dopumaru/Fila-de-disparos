342e64dcec0f436dd87e456249f8db667f2271f2
Dopumaru <caio.10silva@hotmail.com>
2026-02-20 14:21:27 -0300

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,11 +1,9 @@
-// redis.js
 require("dotenv").config();
-
 module.exports = {
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: Number(process.env.REDIS_PORT) || 6379,
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
   username: process.env.REDIS_USERNAME || "default",
-  password: process.env.REDIS_PASSWORD || undefined,
+  password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
   enableReadyCheck: true,
 };
